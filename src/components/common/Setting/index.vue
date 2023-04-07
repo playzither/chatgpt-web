69f86daa6def96e68b3278e0beb442d1668a6147
flow2000 <1982989137@qq.com>
2023-04-07 17:08:49 +0800

    增加免责声明

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -4,6 +4,7 @@ import { NModal, NTabPane, NTabs } from 'naive-ui'
 import General from './General.vue'
 import Advanced from './Advanced.vue'
 import About from './About.vue'
+import Terms from './Terms.vue'
 import { useAuthStore } from '@/store'
 import { SvgIcon } from '@/components/common'
 
@@ -63,6 +64,13 @@ const show = computed({
             <span class="ml-2">{{ $t('setting.config') }}</span>
           </template>
           <About />
+        </NTabPane>
+        <NTabPane name="Terms" tab="Terms">
+          <template #tab>
+            <SvgIcon class="text-lg" icon="mdi:paper-edit-outline" />
+            <span class="ml-2">{{ $t('setting.terms') }}</span>
+          </template>
+          <Terms />
         </NTabPane>
       </NTabs>
     </div>
